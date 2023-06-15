c1d3f24e62d021bf8673207ef3cfacc61186282a
antoniolg <antonio@devexperto.com>
2023-06-15 11:55:08 +0200

    Módulo 9: Listado de tareas

--- modulo9/src/main/java/com/devexperto/modulo9/MainActivity.kt
+++ modulo9/src/main/java/com/devexperto/modulo9/MainActivity.kt
@@ -3,26 +3,11 @@ package com.devexperto.modulo9
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
+import com.devexperto.modulo9.ui.screens.tasks.TasksScreen
 import com.devexperto.modulo9.ui.theme.KotlinAndroid2023Theme
 
 class MainActivity : ComponentActivity() {
@@ -35,44 +20,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Box(
-                        contentAlignment = Alignment.Center
-                    ) {
-                        LoginForm(
-                            modifier = Modifier
-                                .background(Color.LightGray)
-                                .padding(16.dp)
-                        )
-                    }
+                    TasksScreen()
                 }
             }
-        }
-    }
-}
-
-@Composable
-fun LoginForm(modifier: Modifier = Modifier) {
-    var user by remember { mutableStateOf("") }
-    var password by remember { mutableStateOf("") }
-    val enabled = user.isNotEmpty() && password.isNotEmpty()
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterVertically),
-        modifier = modifier
-    ) {
-        TextField(
-            value = user,
-            onValueChange = { user = it }
-        )
-        TextField(
-            value = password,
-            onValueChange = { password = it }
-        )
-        Button(
-            onClick = { user = ""; password = "" },
-            enabled = enabled
-        ) {
-            Text(text = "Login")
         }
     }
 }
---
+++ modulo9/src/main/java/com/devexperto/modulo9/ui/screens/tasks/TasksScreen.kt
@@ -0,0 +1,75 @@
+package com.devexperto.modulo9.ui.screens.tasks
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import com.devexperto.modulo9.App
+import com.devexperto.modulo9.data.TaskRepository
+import com.devexperto.modulo9.domain.AddTaskUseCase
+import com.devexperto.modulo9.domain.GetTasksUseCase
+import com.devexperto.modulo9.domain.UpdateTaskUseCase
+import com.devexperto.modulo9.framework.RoomTaskLocalDataSource
+
+@Composable
+fun TasksScreen(viewModel: TasksViewModel = buildTasksViewModel()) {
+    val tasks by viewModel.items.collectAsState(emptyList())
+
+    Column(
+        modifier = Modifier.padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+
+        var newTask by remember { mutableStateOf("") }
+        OutlinedTextField(
+            value = newTask,
+            onValueChange = { newTask = it },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Button(
+            onClick = { viewModel.onTaskAdd(newTask) },
+            modifier = Modifier.align(Alignment.End)
+        ) {
+            Text(text = "Add Task")
+        }
+
+        LazyColumn {
+            items(tasks) { task ->
+                Text(
+                    text = task.title,
+                    modifier = Modifier.padding(vertical = 8.dp)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun buildTasksViewModel(): TasksViewModel {
+    val application = LocalContext.current.applicationContext
+
+    val taskRepository = TaskRepository(
+        RoomTaskLocalDataSource((application as App).db.taskDao())
+    )
+    return TasksViewModel(
+        GetTasksUseCase(taskRepository),
+        AddTaskUseCase(taskRepository),
+        UpdateTaskUseCase(taskRepository)
+    )
+}
