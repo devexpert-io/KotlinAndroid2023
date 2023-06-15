5b3f55c76be855b066c6d1f05af5fa3edcea1497
antoniolg <antonio@devexperto.com>
2023-06-15 11:22:36 +0200

    Módulo 9: Composable con modifier para Login Form

--- modulo9/src/main/java/com/devexperto/modulo9/MainActivity.kt
+++ modulo9/src/main/java/com/devexperto/modulo9/MainActivity.kt
@@ -3,16 +3,21 @@ package com.devexperto.modulo9
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.devexperto.modulo9.ui.theme.KotlinAndroid2023Theme
 
@@ -26,21 +31,14 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Column(
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterVertically)
+                    Box(
+                        contentAlignment = Alignment.Center
                     ) {
-                        TextField(
-                            value = "user",
-                            onValueChange = {}
+                        LoginForm(
+                            modifier = Modifier
+                                .background(Color.LightGray)
+                                .padding(16.dp)
                         )
-                        TextField(
-                            value = "password",
-                            onValueChange = {}
-                        )
-                        Button(onClick = {}) {
-                            Text(text = "Login")
-                        }
                     }
                 }
             }
@@ -48,3 +46,23 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Composable
+fun LoginForm(modifier: Modifier = Modifier) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterVertically),
+        modifier = modifier
+    ) {
+        TextField(
+            value = "user",
+            onValueChange = {}
+        )
+        TextField(
+            value = "password",
+            onValueChange = {}
+        )
+        Button(onClick = {}) {
+            Text(text = "Login")
+        }
+    }
+}
