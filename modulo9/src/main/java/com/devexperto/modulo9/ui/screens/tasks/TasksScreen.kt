dd3ffd3a27357e99dca1267827b2c0df3b117af4
antoniolg <antonio@devexperto.com>
2023-06-15 12:08:06 +0200

    Módulo 9: Listado de tareas - Reorganizar Composables

--- modulo9/src/main/java/com/devexperto/modulo9/ui/screens/tasks/TasksScreen.kt
+++ modulo9/src/main/java/com/devexperto/modulo9/ui/screens/tasks/TasksScreen.kt
@@ -3,10 +3,12 @@ package com.devexperto.modulo9.ui.screens.tasks
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyItemScope
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
@@ -23,13 +25,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import com.devexperto.modulo9.App
+import com.devexperto.modulo9.data.Task
 import com.devexperto.modulo9.data.TaskRepository
 import com.devexperto.modulo9.domain.AddTaskUseCase
 import com.devexperto.modulo9.domain.GetTasksUseCase
 import com.devexperto.modulo9.domain.UpdateTaskUseCase
 import com.devexperto.modulo9.framework.RoomTaskLocalDataSource
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun TasksScreen(viewModel: TasksViewModel = buildTasksViewModel()) {
     val tasks by viewModel.items.collectAsState(emptyList())
@@ -38,39 +40,53 @@ fun TasksScreen(viewModel: TasksViewModel = buildTasksViewModel()) {
         modifier = Modifier.padding(16.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
-
-        var newTask by remember { mutableStateOf("") }
-        OutlinedTextField(
-            value = newTask,
-            onValueChange = { newTask = it },
-            modifier = Modifier.fillMaxWidth()
-        )
-
-        Button(
-            onClick = { viewModel.onTaskAdd(newTask) },
-            modifier = Modifier.align(Alignment.End)
-        ) {
-            Text(text = "Add Task")
-        }
+        NewTaskForm(onNewTask = viewModel::onTaskAdd)
 
         LazyColumn {
             items(tasks, key = { it.id }) { task ->
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier.animateItemPlacement()
-                ) {
-                    Text(
-                        text = task.title,
-                        modifier = Modifier
-                            .padding(vertical = 8.dp)
-                            .weight(1f)
-                    )
-                    Checkbox(
-                        checked = task.completed,
-                        onCheckedChange = { viewModel.onTaskCheck(task.copy(completed = !task.completed)) })
-                }
+                TaskItem(
+                    task = task,
+                    onTaskCheck = { viewModel.onTaskCheck(task.copy(completed = it)) }
+                )
             }
         }
+    }
+}
+
+@Composable
+fun ColumnScope.NewTaskForm(onNewTask: (String) -> Unit) {
+    var newTask by remember { mutableStateOf("") }
+    OutlinedTextField(
+        value = newTask,
+        onValueChange = { newTask = it },
+        modifier = Modifier.fillMaxWidth()
+    )
+
+    Button(
+        onClick = { onNewTask(newTask) },
+        modifier = Modifier.align(Alignment.End)
+    ) {
+        Text(text = "Add Task")
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun LazyItemScope.TaskItem(task: Task, onTaskCheck: (Boolean) -> Unit) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.animateItemPlacement()
+    ) {
+        Text(
+            text = task.title,
+            modifier = Modifier
+                .padding(vertical = 8.dp)
+                .weight(1f)
+        )
+        Checkbox(
+            checked = task.completed,
+            onCheckedChange = onTaskCheck
+        )
     }
 }
 
