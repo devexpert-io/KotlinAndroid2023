be2f62d2e4093ea89616c21d38fc242d76f883c8
antoniolg <antonio@devexperto.com>
2023-06-16 12:53:46 +0200

    Módulo 6: Código final ejercicio

--- modulo6/src/main/java/com/devexperto/modulo6/ejercicio/EjercicioActivity.kt
+++ modulo6/src/main/java/com/devexperto/modulo6/ejercicio/EjercicioActivity.kt
@@ -1,11 +1,20 @@
 package com.devexperto.modulo6.ejercicio
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.devexperto.modulo6.databinding.ActivityEjercicioBinding
 
+private const val CALL_PHONE_PERMISSION_REQUEST_CODE = 1
+
 class EjercicioActivity : AppCompatActivity() {
-    lateinit var binding: ActivityEjercicioBinding
+    private lateinit var binding: ActivityEjercicioBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,9 +22,29 @@ class EjercicioActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.btnLlamar.setOnClickListener {
-
+            makePhoneCall()
         }
     }
 
+    private fun makePhoneCall() {
+        val phoneNumber = "+59177752810"
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
+            val intent = Intent(Intent.ACTION_CALL)
+            intent.data = Uri.parse("tel:$phoneNumber")
+            startActivity(intent)
+        } else {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CALL_PHONE), CALL_PHONE_PERMISSION_REQUEST_CODE)
+        }
+    }
 
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == CALL_PHONE_PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                makePhoneCall()
+            } else {
+                Toast.makeText(this, "Permiso denegado", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 }
