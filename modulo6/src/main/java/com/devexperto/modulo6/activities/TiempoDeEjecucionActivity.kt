dba7881e1dd5dd8121404eea741ee78652d14841
antoniolg <antonio@devexperto.com>
2023-06-16 12:44:50 +0200

    Módulo 6: Ejemplo tiempo de ejecución, múltiples permisos

--- modulo6/src/main/java/com/devexperto/modulo6/activities/TiempoDeEjecucionActivity.kt
+++ modulo6/src/main/java/com/devexperto/modulo6/activities/TiempoDeEjecucionActivity.kt
@@ -1,10 +1,14 @@
 package com.devexperto.modulo6.activities
 
 import android.Manifest
+import android.app.AlertDialog
+import android.content.Intent
 import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.devexperto.modulo6.databinding.ActivityTiempoDeEjecucionBinding
@@ -16,6 +20,12 @@ class TiempoDeEjecucionActivity : AppCompatActivity() {
         private const val CONTACTS_PERMISSION_REQUEST_CODE = 1
         private const val LOCATION_PERMISSION_REQUEST_CODE = 2
         private const val CAMERA_PERMISSION_REQUEST_CODE = 3
+        private const val MULTIPLE_PERMISSION_REQUEST_CODE = 4
+
+        var requiredPermissions = arrayOf(
+            Manifest.permission.CAMERA,
+            Manifest.permission.RECORD_AUDIO
+        )
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,31 +37,75 @@ class TiempoDeEjecucionActivity : AppCompatActivity() {
             requestPermission(Manifest.permission.READ_CONTACTS, CONTACTS_PERMISSION_REQUEST_CODE)
         }
         binding.btnAccederUbicacion.setOnClickListener {
-            requestPermission(Manifest.permission.ACCESS_FINE_LOCATION, LOCATION_PERMISSION_REQUEST_CODE)
+            requestPermission(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                LOCATION_PERMISSION_REQUEST_CODE
+            )
         }
         binding.btnAccederCamara.setOnClickListener {
             requestPermission(Manifest.permission.CAMERA, CAMERA_PERMISSION_REQUEST_CODE)
+        }
+        binding.btnAccederMultiplesPermisos.setOnClickListener {
+            requestPermissions()
         }
     }
 
     private fun requestPermission(permission: String, requestCode: Int) {
         if (isPermissionGranted(permission)) {
             showToast("El permiso ya ha sido otorgado")
-            when(requestCode) {
-                CONTACTS_PERMISSION_REQUEST_CODE -> { getContacts() }
-                LOCATION_PERMISSION_REQUEST_CODE -> { getLocalization() }
-                CAMERA_PERMISSION_REQUEST_CODE -> { openCamera() }
+            when (requestCode) {
+                CONTACTS_PERMISSION_REQUEST_CODE -> {
+                    getContacts()
+                }
+
+                LOCATION_PERMISSION_REQUEST_CODE -> {
+                    getLocalization()
+                }
+
+                CAMERA_PERMISSION_REQUEST_CODE -> {
+                    openCamera()
+                }
             }
         } else {
             ActivityCompat.requestPermissions(this, arrayOf(permission), requestCode)
         }
     }
 
-    private fun isPermissionGranted(permission: String): Boolean {
-        return ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED
+    private fun requestPermissions() {
+        val permissionsToRequest = mutableListOf<String>()
+        for (permission in requiredPermissions) {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    permission
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                permissionsToRequest.add(permission)
+            }
+        }
+        if (permissionsToRequest.isNotEmpty()) {
+            ActivityCompat.requestPermissions(
+                this,
+                permissionsToRequest.toTypedArray(),
+                MULTIPLE_PERMISSION_REQUEST_CODE
+            )
+        } else {
+            showToast("Los permisos ya ha sido otorgados")
+            getFuncionalidadMultiplesPermisos()
+        }
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+    private fun isPermissionGranted(permission: String): Boolean {
+        return ContextCompat.checkSelfPermission(
+            this,
+            permission
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         when (requestCode) {
             CONTACTS_PERMISSION_REQUEST_CODE -> {
@@ -62,6 +116,7 @@ class TiempoDeEjecucionActivity : AppCompatActivity() {
                     showToast("Permiso de contactos denegado")
                 }
             }
+
             LOCATION_PERMISSION_REQUEST_CODE -> {
                 if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     showToast("Permiso de localización otorgado")
@@ -70,12 +125,29 @@ class TiempoDeEjecucionActivity : AppCompatActivity() {
                     showToast("Permiso de localización denegado")
                 }
             }
+
             CAMERA_PERMISSION_REQUEST_CODE -> {
                 if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     showToast("Permiso de camara otorgado")
                     openCamera()
                 } else {
                     showToast("Permiso de camara denegado")
+                }
+            }
+
+            MULTIPLE_PERMISSION_REQUEST_CODE -> {
+                var allPermissionsGranted = true
+                for (result in grantResults) {
+                    if (result != PackageManager.PERMISSION_GRANTED) {
+                        allPermissionsGranted = false
+                        break
+                    }
+                }
+                if (allPermissionsGranted) {
+                    showToast("Todos los permisos fueron otorgados")
+                    getFuncionalidadMultiplesPermisos()
+                } else {
+                    showPermissionDialog()
                 }
             }
         }
@@ -93,9 +165,29 @@ class TiempoDeEjecucionActivity : AppCompatActivity() {
         showToast("Funcionalidad de localización")
     }
 
+    private fun getFuncionalidadMultiplesPermisos() {
+        showToast("Funcionalidad Multiples permisos")
+    }
 
     private fun showToast(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 
+    private fun showPermissionDialog() {
+        AlertDialog.Builder(this)
+            .setTitle("Se requiere permisos")
+            .setMessage("Se necesitan algunos permisos para poder usar esta aplicación sin ningún problema.")
+            .setPositiveButton("Otorgar") { dialog, _ ->
+                dialog.cancel()
+                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                val uri = Uri.fromParts("package", this.packageName, null)
+                intent.data = uri
+                startActivity(intent)
+            }
+            .setNegativeButton("Cancelar") { dialog, _ ->
+                dialog.dismiss()
+            }
+            .show()
+    }
 }
