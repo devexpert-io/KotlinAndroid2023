7af37846cdf411c3576ec184726864678a47bbdd
antoniolg <antonio@devexperto.com>
2023-06-16 12:09:13 +0200

    Módulo 6: Ejemplo sin permisos, capturar desde camara

--- modulo6/src/main/java/com/devexperto/modulo6/activities/SinPermisosActivity.kt
+++ modulo6/src/main/java/com/devexperto/modulo6/activities/SinPermisosActivity.kt
@@ -1,9 +1,15 @@
 package com.devexperto.modulo6.activities
 
+import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.graphics.Bitmap
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.BundleCompat
 import com.devexperto.modulo6.databinding.ActivitySinPermisosBinding
 
 class SinPermisosActivity : AppCompatActivity() {
@@ -21,7 +27,7 @@ class SinPermisosActivity : AppCompatActivity() {
             compartirTexto("Hola desde DevExperto")
         }
         binding.btnAbrirCamara.setOnClickListener {
-
+            abrirCamara()
         }
     }
 
@@ -37,4 +43,24 @@ class SinPermisosActivity : AppCompatActivity() {
         intent.putExtra(Intent.EXTRA_TEXT, texto)
         startActivity(Intent.createChooser(intent, "Compartir"))
     }
+
+    private fun abrirCamara() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        try {
+            galleryActivityResultLauncher.launch(takePictureIntent)
+        } catch (e: ActivityNotFoundException) {
+            Log.e("SinPermisosActivity", "No se pudo abrir la cámara", e)
+        }
+    }
+
+    private val galleryActivityResultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val data: Intent? = result.data
+                val imageBitmap = data?.extras?.let {
+                    BundleCompat.getParcelable(it, "data", Bitmap::class.java)
+                }
+                binding.ivImagen.setImageBitmap(imageBitmap)
+            }
+        }
 }
