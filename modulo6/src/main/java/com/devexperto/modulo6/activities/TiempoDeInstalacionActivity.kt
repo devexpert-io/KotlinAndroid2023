7a30fb08b50c42e6997e6400ae406262ea152b2d
antoniolg <antonio@devexperto.com>
2023-06-16 12:10:43 +0200

    Módulo 6: Ejemplo tiempo de instalación, estado de red

--- modulo6/src/main/java/com/devexperto/modulo6/activities/TiempoDeInstalacionActivity.kt
+++ modulo6/src/main/java/com/devexperto/modulo6/activities/TiempoDeInstalacionActivity.kt
@@ -1,5 +1,8 @@
 package com.devexperto.modulo6.activities
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.devexperto.modulo6.databinding.ActivityTiempoDeInstalacionBinding
@@ -12,13 +15,26 @@ class TiempoDeInstalacionActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.btnEstadoDeRed.setOnClickListener {
-
+            binding.tvEstadoDeRed.text = if (obtenerDisponibilidadDeLaRed(this)) "Conectado" else "Desconectado"
         }
         binding.btnVibracion.setOnClickListener {
 
         }
         binding.btnAccederAInternet.setOnClickListener {
 
+        }
+    }
+
+    private fun obtenerDisponibilidadDeLaRed(context: Context): Boolean {
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val conn = connectivityManager.activeNetwork ?: return false
+        val activeNewtwork = connectivityManager.getNetworkCapabilities(conn) ?: return false
+        return when {
+            activeNewtwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            activeNewtwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+            activeNewtwork.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+            activeNewtwork.hasTransport(NetworkCapabilities.TRANSPORT_BLUETOOTH) -> true
+            else -> false
         }
     }
 }
