ecb9e514565cc5fd785433b189ca8b151348c7f5
antoniolg <antonio@devexperto.com>
2023-06-16 12:39:28 +0200

    Módulo 6: Ejemplo tiempo de ejecución, permiso localización

--- modulo6/src/main/java/com/devexperto/modulo6/activities/TiempoDeEjecucionActivity.kt
+++ modulo6/src/main/java/com/devexperto/modulo6/activities/TiempoDeEjecucionActivity.kt
@@ -2,9 +2,9 @@ package com.devexperto.modulo6.activities
 
 import android.Manifest
 import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.devexperto.modulo6.databinding.ActivityTiempoDeEjecucionBinding
@@ -14,6 +14,7 @@ class TiempoDeEjecucionActivity : AppCompatActivity() {
 
     companion object {
         private const val CONTACTS_PERMISSION_REQUEST_CODE = 1
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 2
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,19 +25,23 @@ class TiempoDeEjecucionActivity : AppCompatActivity() {
         binding.btnAccederContactos.setOnClickListener {
             requestPermission(Manifest.permission.READ_CONTACTS, CONTACTS_PERMISSION_REQUEST_CODE)
         }
+        binding.btnAccederUbicacion.setOnClickListener {
+            requestPermission(Manifest.permission.ACCESS_FINE_LOCATION, LOCATION_PERMISSION_REQUEST_CODE)
+        }
     }
 
-    @Suppress("SameParameterValue")
     private fun requestPermission(permission: String, requestCode: Int) {
         if (isPermissionGranted(permission)) {
             showToast("El permiso ya ha sido otorgado")
             when(requestCode) {
                 CONTACTS_PERMISSION_REQUEST_CODE -> { getContacts() }
+                LOCATION_PERMISSION_REQUEST_CODE -> { getLocalization() }
             }
         } else {
             ActivityCompat.requestPermissions(this, arrayOf(permission), requestCode)
         }
     }
+
 
     private fun isPermissionGranted(permission: String): Boolean {
         return ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED
@@ -53,11 +58,23 @@ class TiempoDeEjecucionActivity : AppCompatActivity() {
                     showToast("Permiso de contactos denegado")
                 }
             }
+            LOCATION_PERMISSION_REQUEST_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    showToast("Permiso de localización otorgado")
+                    getLocalization()
+                } else {
+                    showToast("Permiso de localización denegado")
+                }
+            }
         }
     }
 
     private fun getContacts() {
         showToast("Funcionalidad de contactos")
+    }
+
+    private fun getLocalization() {
+        showToast("Funcionalidad de localización")
     }
 
     private fun showToast(message: String) {
